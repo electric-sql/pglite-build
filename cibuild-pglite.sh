400bb2723f787847ae57764bc638a539bed03ef8
pmp-p <mail.peny@free.fr>
2024-06-06 10:37:15 +0200

    missing file

---
+++ cibuild-pglite.sh
@@ -0,0 +1,28 @@
+    if [ -d pglite ]
+    then
+        echo using local
+    else
+        git clone --no-tags --depth 1 --single-branch --branch pglite-build https://github.com/electric-sql/pglite pglite
+    fi
+
+    pushd pglite/packages/pglite
+    npm install
+    npm run build
+    popd
+
+    #> pglite/packages/pglite/release/postgres.js
+    cp ${WEBROOT}/postgres.{js,data,wasm} pglite/packages/pglite/release/
+    cp ${WEBROOT}/libecpg.so pglite/packages/pglite/release/postgres.so
+    mv pglite/packages/pglite/release/postgres.js pglite/packages/pglite/release/pgbuild.js
+
+    cat > pglite/packages/pglite/release/share.js <<END
+
+    function loadPgShare(module, require) {
+        console.warn("share.js: loadPgShare");
+    }
+
+    export default loadPgShare;
+END
+
+    cat pgbuild.js > pglite/packages/pglite/release/postgres.js
+
