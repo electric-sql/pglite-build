d331329d0d6af14c476fa37d3f754f75aaabe49c
pmp-p <mail.peny@free.fr>
2025-02-18 16:10:03 +0100

    versionned pglites file

--- ci-17_2_WASM.sh
+++ ci-17_2_WASM.sh
@@ -2,13 +2,24 @@
 export WORKSPACE=$(pwd)
 export PG_VERSION=${PG_VERSION:-REL_17_2_WASM}
 
+#
+#[ -f postgresql-${PG_VERSION}/configure ] \
+# || git clone --no-tags --depth 1 --single-branch --branch ${PG_VERSION} https://github.com/pygame-web/postgres postgresql-${PG_VERSION}
+#
+
 [ -f postgresql-${PG_VERSION}/configure ] \
- || git clone --no-tags --depth 1 --single-branch --branch ${PG_VERSION} https://github.com/pygame-web/postgres postgresql-${PG_VERSION}
+ || git clone --no-tags --depth 1 --single-branch --branch ${PG_VERSION} https://github.com/electric-sql/postgres-pglite postgresql-${PG_VERSION}
 
 chmod +x portable/*.sh wasm-build/*.sh
 cp -R wasm-build* patches-${PG_VERSION} postgresql-${PG_VERSION}/
 
-[ -d postgresql-${PG_VERSION}/pglite ] || cp -Rv pglite postgresql-${PG_VERSION}/
+if [ -d postgresql-${PG_VERSION}/pglite ]
+then
+    echo "using local pglite files"
+else
+    mkdir -p postgresql-${PG_VERSION}/pglite
+    cp -Rv pglite-${PG_VERSION}/* postgresql-${PG_VERSION}/pglite/
+fi
 
 pushd postgresql-${PG_VERSION}
     ${WORKSPACE}/portable/portable.sh
