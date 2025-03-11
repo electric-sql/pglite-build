4746239f7b7c878306a3516e824bc3243d4e419a
pmp-p <mail.peny@free.fr>
2025-03-11 10:35:03 +0100

    possible schema for releases directories

--- wasm-build/build-withdocker.sh
+++ wasm-build/build-withdocker.sh
@@ -18,12 +18,36 @@ if [[ -z "$SDKROOT" || -z "$PG_VERSION" ]]; then
 fi
 
 
+# release directory
+mkdir -p ${WORKSPACE}/dist
+
+# lib postgres "libpgcore.a" backend with no main/main.o tcop/postgres.o.
+mkdir -p ${WORKSPACE}/dist/postgres-wasm
+
+# full wasi postgres ( no extensions , only plpgsql and vector
+mkdir -p ${WORKSPACE}/dist/postgres-wasi
+
+# node/bun app with RAWFS support ( direct disk access from Node/Bun )
+mkdir -p ${WORKSPACE}/dist/postgres-emsdk
+
+# node/bun pglite with RAWFS support ( direct disk access from Node/Bun )
+mkdir -p ${WORKSPACE}/dist/pglite-emsdk
+
+# web+node pglite, smaller node fs mount on subfolders no direct disk access.
+mkdir -p ${WORKSPACE}/dist/pglite-sandbox
+
+
+# web only pglite - smallest - no node fs mount.
+mkdir -p ${WORKSPACE}/dist/pglite-web
+
+
 
 docker run \
   --rm \
   -e SDKROOT=$SDKROOT \
   -e PG_VERSION=${PG_VERSION} \
   -e PG_BRANCH=${PG_BRANCH} \
+  -v /tmp/sdk:${WORKSPACE}/dist
   -v .:${WORKSPACE} \
   $IMG_NAME:$IMG_TAG \
   bash /workspace/wasm-build.sh ${WHAT:-"contrib extra"}
