fa27aa904b89d010ce623b6c9561f5567c4c0ce9
pmp-p <mail.peny@free.fr>
2025-03-17 22:36:44 +0000

    Deploying to gh-pages from @ electric-sql/pglite-build@9410d991c59e00b5b35bc8599592c5e16cfc78ba 🚀

--- dist/pglite.js
+++ dist/pglite.js
@@ -65,7 +65,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp1mkr1myl.js
+// include: /tmp/tmp1qchytr9.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -282,21 +282,21 @@ Module['FS_createPath']("/tmp/pglite/share/postgresql", "tsearch_data", true, tr
 
   })();
 
-// end include: /tmp/tmp1mkr1myl.js
-// include: /tmp/tmppe85rpvr.js
+// end include: /tmp/tmp1qchytr9.js
+// include: /tmp/tmphew6pcfp.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmppe85rpvr.js
-// include: /tmp/tmpy7p1ww0y.js
+  // end include: /tmp/tmphew6pcfp.js
+// include: /tmp/tmpom_zatc4.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpy7p1ww0y.js
+  // end include: /tmp/tmpom_zatc4.js
 
 
 // Sometimes an existing Module object exists with properties
